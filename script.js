c900c5681030339814b96284d9f84f14f085dec6
Ivan Antipenko <JohnnAnt@yandex.ru>
2022-03-21 23:46:44 +0300

    Вроде привел код у вдобоваримый вид

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ const popupAdd = document.querySelector('.popup-add'); // Попап места
 const popupViewer = document.querySelector('.popup-viewer'); // Попап изображения
 const popupViewerImage = document.querySelector('.popup__image'); // Картинка попапа
 const popupImageDescription = document.querySelector('.popup__image-description'); // Подпись картинки попапа
-const editProfileButton = document.querySelector('.profile__edit-button'); // Кнопка редактирования профиля
-const addPlaceButton = document.querySelector('.profile__add-button') // Кнопка добавления карточки
-const editPopupCloseButton = document.querySelector('.popup-edit__close-button'); // Кнопка закрытия попапа edit
-const addPopupCloseButton = document.querySelector('.popup-add__close-button'); // Кнопка закрытия попапа add
-const imagePopupCloseButton = document.querySelector('.popup-image__close-button'); // Кнопка закрытия попапа viewer
+const buttonEditProfile = document.querySelector('.profile__edit-button'); // Кнопка редактирования профиля
+const buttonAddPlace = document.querySelector('.profile__add-button') // Кнопка добавления карточки
+const buttonEditPopupClose = document.querySelector('.popup-edit__close-button'); // Кнопка закрытия попапа edit
+const buttonAddPopupClose = document.querySelector('.popup-add__close-button'); // Кнопка закрытия попапа add
+const buttonImagePopupClose = document.querySelector('.popup-image__close-button'); // Кнопка закрытия попапа viewer
 const popupFormEditProfile = document.querySelector('.popup__form_edit-profile'); // Форма профиля
 const popupFormEditProfileFieldName = document.querySelector('.popup__form-input_field-name'); // Инпут для имени
 const popupFormEditProfileFieldAbout = document.querySelector('.popup__form-input_field-about'); // Инпут "О себе"
@@ -21,6 +21,35 @@ const popupFormPlaceLinkField = document.querySelector('.popup__form-input_place
 const profileName = document.querySelector('.profile__name'); // Имя профиля
 const profileAbout = document.querySelector('.profile__about'); // "О себе"
 //===============================================================
+
+// Конструктор карточек
+function creatingCard(cardName, cardLink) {
+  const cardElement = cardConstructor.querySelector('.element').cloneNode(true);
+  const elementImageContent = cardElement.querySelector('.element__image');
+  cardElement.querySelector('.element__title').textContent = cardName;
+  elementImageContent.src = cardLink;
+  elementImageContent.alt = cardLink;
+
+  cardElement.querySelector('.element__like').addEventListener('click', (evt) => {
+    evt.target.classList.toggle('element__like_active')
+  });
+  cardElement.querySelector('.element__delete-icon').addEventListener('click', (evt) => {
+    evt.target.closest('.element').remove();
+  });
+  cardElement.querySelector('.element__image').addEventListener('click', () => {
+    openingPopup(popupViewer);
+    popupViewerImage.src = cardElement.querySelector('.element__image').src;
+    popupImageDescription.textContent = cardElement.querySelector('.element__title').textContent;
+    popupViewerImage.alt = popupImageDescription.textContent;
+  });
+  return cardElement;
+};
+//===============================================================
+
+// Функция выведения карточки на экран
+function renderCard(cardElement) {
+  cardsSection.prepend(cardElement);
+};
 
 // Инициализация карточек из массива
 const initialCards = [{
@@ -48,29 +77,25 @@ const initialCards = [{
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
-initialCards.forEach((card) => {
-  const cardElement = cardConstructor.querySelector('.element').cloneNode(true);
-  cardElement.querySelector('.element__title').textContent = card.name;
-  cardElement.querySelector('.element__image').src = card.link;
-  cardElement.querySelector('.element__image').alt = card.name;
-  enableIconsCard(cardElement);
-  cardsSection.append(cardElement);
+initialCards.reverse().forEach((card) => {
+  renderCard(creatingCard(card.name, card.link));
+});
+//===============================================================
+
+// Добавление карточек
+popupFormAddPlace.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  renderCard(creatingCard(popupFormPlaceNameField.value, popupFormPlaceLinkField.value));
+  popupFormPlaceNameField.value = "";
+  popupFormPlaceLinkField.value = "";
 });
 //===============================================================
 
 // Функции открытия и закрытия попапов
 function openingEditPopup(popup) {
-  popup.classList.add('popup_opened');
-  // Автозаполнение полей ввода
+  openingPopup(popup);
   popupFormEditProfileFieldName.value = profileName.textContent;
   popupFormEditProfileFieldAbout.value = profileAbout.textContent;
-  // Сохранение изменений
-  popupFormEditProfile.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    profileName.textContent = popupFormEditProfileFieldName.value;
-    profileAbout.textContent = popupFormEditProfileFieldAbout.value;
-    document.querySelector('.popup').classList.remove('popup_opened');
-  });
 };
 
 function openingPopup(popup) {
@@ -82,54 +107,32 @@ function closePopup(popup) {
 };
 //===============================================================
 
-// Функция добавления интерактива иконкам
-function enableIconsCard(cardElement) {
-  cardElement.querySelector('.element__like').addEventListener('click', (evt) => {
-    evt.target.classList.toggle('element__like_active')
-  });
-  cardElement.querySelector('.element__delete-icon').addEventListener('click', () => {
-    const deletingCard = cardElement.querySelector('.element__delete-icon').closest('.element');
-    deletingCard.remove();
-  });
-  cardElement.querySelector('.element__image').addEventListener('click', () => {
-    openingPopup(popupViewer);
-    popupViewerImage.src = cardElement.querySelector('.element__image').src;
-    popupImageDescription.textContent = cardElement.querySelector('.element__title').textContent;
-  });
-};
-//===============================================================
-
-// Добавление карточек
-popupFormAddPlace.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  const cardElement = cardConstructor.querySelector('.element').cloneNode(true);
-  cardElement.querySelector('.element__title').textContent = popupFormPlaceNameField.value;
-  cardElement.querySelector('.element__image').src = popupFormPlaceLinkField.value;
-  cardElement.querySelector('.element__image').alt = popupFormPlaceNameField.value;
-  enableIconsCard(cardElement);
-  cardsSection.prepend(cardElement);
-  popupFormPlaceNameField.value = "";
-  popupFormPlaceLinkField.value = "";
-});
-//===============================================================
-
 // Открытие и закрытие попапа edit
-editProfileButton.addEventListener('click', () => {
+buttonEditProfile.addEventListener('click', () => {
   openingEditPopup(popupEdit);
 });
-editPopupCloseButton.addEventListener('click', () => {
+buttonEditPopupClose.addEventListener('click', () => {
   closePopup(popupEdit);
 });
 //===============================================================
 
-// Открытие и закрытие попапов
-addPlaceButton.addEventListener('click', () => {
+// Открытие и закрытие остальных попапов
+buttonAddPlace.addEventListener('click', () => {
   openingPopup(popupAdd);
 });
-addPopupCloseButton.addEventListener('click', () => {
+buttonAddPopupClose.addEventListener('click', () => {
   closePopup(popupAdd);
 });
-imagePopupCloseButton.addEventListener('click', () => {
+buttonImagePopupClose.addEventListener('click', () => {
   closePopup(popupViewer);
 });
 //===============================================================
+
+// Сохранение изменений профиля
+popupFormEditProfile.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  profileName.textContent = popupFormEditProfileFieldName.value;
+  profileAbout.textContent = popupFormEditProfileFieldAbout.value;
+  closePopup(popupEdit);
+});
+//==========================================================
